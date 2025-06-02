91881f82cb0060e0a883a9bd6a4918797eb57de8
janvoser <voserjan@students.zhaw.ch>
2025-06-02 17:06:18 +0200

    überarbeitet

--- src/routes/exercises/+page.server.js
+++ src/routes/exercises/+page.server.js
@@ -2,5 +2,5 @@ import db from "$lib/db.js";
 
 export async function load() {
   const exercises = await db.getExercises();
-  return { exercises };
+  return { exercises }; // Diese Übungen werden an die +page.svelte weitergegeben
 }
